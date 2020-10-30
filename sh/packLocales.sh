62e5ffdd995830eceeec3da20bcd10c903d0e9bc
EdChdX <edchdx@pm.me>
2020-10-30 17:37:04 +0800

    Cleanup translations

--- sh/packLocales.sh
+++ sh/packLocales.sh
@@ -1,7 +1,12 @@
 #!~/bin/bash
 cd ../_locales
-mv ./zh_Hans ./zh_CN
-mv ./zh_Hant ./zh_TW
-mv ./zh_Hant_HK ./zh_HK
+mkdir zh_CN
+mkdir zh_TW
+mkdir zh_HK
+mv -f ./zh_Hans/* ./zh_CN
+mv -f ./zh_Hant/* ./zh_TW
+mv -f ./zh_Hant_HK/* ./zh_HK
+rm -rf zh_Hans
+rm -rf zh_Hant
+rm -rf zh_Hant_HK
 exit
-
