c8e871e3ee6be7e1dee7e15219b82965286e29b5
EdChdX <15964383+EdChdX@users.noreply.github.com>
2020-10-30 17:25:22 +0000

    Basic design update

--- js/base/base.js
+++ js/base/base.js
@@ -1,5 +1,38 @@
 "use strict";
 
+// Pre-announce variables
+var bgContainer, bgImgEl, iconMgr;
+
+// Image resizer
+var bgResize = function () {
+	let cWidth = bgContainer.clientWidth;
+	let cHeight = bgContainer.clientHeight;
+	let cRatio = cWidth / cHeight;
+	bgImgEl.forEach(function (e) {
+		let nWidth = e.naturalWidth;
+		let nHeight = e.naturalHeight;
+		let nRatio = nWidth / nHeight;
+		// Clean up previous size
+		e.style.width = "";
+		e.style.height = "";
+		if (!(nWidth * nHeight > 0)) {
+			//console.warn("Failed resizing element: %o", e);
+		} else if (nRatio > cRatio) {
+			e.style.width = (cHeight * nRatio).toString() + "px";
+		} else if (nRatio < cRatio) {
+			e.style.height = (cWidth / nRatio).toString() + "px";
+		};
+	});
+}
+// Element resizer
+var eleResize = function () {
+	bgResize();
+};
+
+// Resizing?
+self.addEventListener("resize", function () {
+	eleResize();
+});
 // Loaded?
 document.addEventListener("readystatechange", function () {
 	switch (this.readyState.toLowerCase()) {
@@ -7,9 +40,31 @@ document.addEventListener("readystatechange", function () {
 			break;
 		};
 		case "interactive": {
+			// Get the elements
+			bgContainer = document.querySelector(".background");
+			bgImgEl = [
+				document.querySelector("#bg-main"),
+				document.querySelector("#bg-tween")
+			];
+			// Resize async
+			new Promise(function (p, r) {
+				eleResize();
+				p();
+			});
+			// Load icons
+			loadIcons("icons/all").then(function (iconMgr) {
+				iconMgr.updateIconsAll();
+				self.iconMgr = iconMgr;
+			});
+			// Disable auxclick
+			document.body.oncontextmenu = function () {
+				return false;
+			};
 			break;
 		};
 		case "complete": {
+			// Resize the elements
+			eleResize();
 			break;
 		};
 	};
