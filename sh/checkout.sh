8606cbb29b1f1698962a242a0cf1e34ba7373e48
EdChdX <15964383+EdChdX@users.noreply.github.com>
2020-12-08 14:03:54 +0000

    mass cleanup

--- sh/checkout.sh
+++ sh/checkout.sh
@@ -4,6 +4,6 @@ bash packLocales.sh
 bash getLocales.sh
 # Pack compressed files
 cd ..
-tree -iFf | grep -i ".bz2" | grep -v "LICENSE" > ./conf/decompress.txt
+tree -iFf | grep -i "\.bz2" | grep -v "LICENSE" > ./conf/decompress.txt
 cd sh
 exit
